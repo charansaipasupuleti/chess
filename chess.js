ffa698140627ac62662dbd38c18ffbc63029397f
charansaipasupuleti <charanpasupuleti08@gmail.com>
2025-10-09 10:40:18 +0530

    version-test 1.0

---
+++ chess.js
@@ -0,0 +1,75 @@
+
+for(let i=0;i<8;i++){
+    let square =  document.createElement('div');
+    document.getElementsByClassName('chessboard')[0].append
+    (square);
+    if(i %2 !==0){
+    square.style.backgroundColor="#81b64c";
+    } else {
+        square.style.backgroundColor ='white';    
+    }
+}for(let i=0;i<8;i++){
+    let square =  document.createElement('div');
+    document.getElementsByClassName('chessboard')[0].append
+    (square);
+    if(i %2 ==0){
+    square.style.backgroundColor="#81b64c";
+    } else {
+        square.style.backgroundColor ='white';    
+    }
+}for(let i=0;i<8;i++){
+    let square =  document.createElement('div');
+    document.getElementsByClassName('chessboard')[0].append
+    (square);
+    if(i %2 !==0){
+    square.style.backgroundColor="#81b64c";
+    } else {
+        square.style.backgroundColor ='white';    
+    }
+}for(let i=0;i<8;i++){
+    let square =  document.createElement('div');
+    document.getElementsByClassName('chessboard')[0].append
+    (square);
+    if(i %2 ==0){
+    square.style.backgroundColor="#81b64c";
+    } else {
+        square.style.backgroundColor ='white';    
+    }
+}for(let i=0;i<8;i++){
+    let square =  document.createElement('div');
+    document.getElementsByClassName('chessboard')[0].append
+    (square);
+    if(i %2 !==0){
+    square.style.backgroundColor="#81b64c";
+    } else {
+        square.style.backgroundColor ='white';    
+    }
+}for(let i=0;i<8;i++){
+    let square =  document.createElement('div');
+    document.getElementsByClassName('chessboard')[0].append
+    (square);
+    if(i %2 ==0){
+    square.style.backgroundColor="#81b64c";
+    } else {
+        square.style.backgroundColor ='white';    
+    }
+}for(let i=0;i<8;i++){
+    let square =  document.createElement('div');
+    document.getElementsByClassName('chessboard')[0].append
+    (square);
+    if(i %2 !==0){
+    square.style.backgroundColor="#81b64c";
+    } else {
+        square.style.backgroundColor ='white';    
+    }
+}for(let i=0;i<8;i++){
+    let square =  document.createElement('div');
+    document.getElementsByClassName('chessboard')[0].append
+    (square);
+    if(i %2 ==0){
+    square.style.backgroundColor="#81b64c";
+    } else {
+        square.style.backgroundColor ='white';    
+    }
+}
+
